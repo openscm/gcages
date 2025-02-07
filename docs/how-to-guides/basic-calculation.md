65e299e58b95b72d2bee479b3f29bbc999787944
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-02-07 15:07:23 +0100

    Set up

---
+++ docs/how-to-guides/basic-calculation.md
@@ -0,0 +1,10 @@
+# How to do a basic calculation
+
+An example of how to do a basic calculation using gcages.
+
+```python
+>>> from gcages.operations import add_two
+
+>>> add_two(3.2, 4.3)
+7.5
+```
---
+++ src/gcages/__init__.py
@@ -0,0 +1,7 @@
+"""
+Global Climate Assessment of Global Emission Scenarios, typically (but not always) produced by Integrated Assessment Models (IAMs).
+"""
+
+import importlib.metadata
+
+__version__ = importlib.metadata.version("gcages")
