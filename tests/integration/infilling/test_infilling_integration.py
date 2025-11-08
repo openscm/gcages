eb9556c543c7de74b1c2daebd35533bdfa13c851
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-11-08 22:00:11 +0800

    Start sketching

--- tests/integration/infilling/test_infilling_integration.py
+++ tests/integration/infilling/test_infilling_integration.py
@@ -2,6 +2,9 @@
 Integration tests of `gcages.infilling`
 """
 
+import pandas as pd
+import pytest
+
 
 def test_basic_silicone():
     assert False
@@ -14,3 +17,109 @@ def test_basic_silicone():
 # - works for multiple scenarios with different missing variables
 # - config passed correctly (infill with silicone in test,
 #   make sure same result comes through via config)
+# - regional infilling
+# - unit handling
+
+
+def test_regional_infilling_silicone(setup_pandas_accessors):
+    # fake infilling db
+    # - CO2 total
+    # - CH4 (infill at regional level)
+    # - sulfur (infill at global level)
+    # infill
+    # check results against using silicone directly
+    openscm_units = pytest.importorskip("openscm_units")
+
+    scen_1 = pd.DataFrame(
+        [
+            [1.0, 2.0, 3.0],
+            [1.0, 1.0, 1.0],
+            [0.0, 1.0, 2.0],
+            [100.0, 220.0, 300.0],
+            [85.0, 200.0, 270.0],
+            [15.0, 20.0, 30.0],
+            [10.0, 20.0, 32.0],
+        ],
+        columns=[2020, 2040, 2050],
+        index=pd.MultiIndex.from_tuples(
+            [
+                ("s1", "CO2", "World", "GtC / yr"),
+                ("s1", "CO2", "R1", "GtC / yr"),
+                ("s1", "CO2", "R2", "GtC / yr"),
+                ("s1", "CH4", "World", "MtCH4 / yr"),
+                ("s1", "CH4", "R1", "MtCH4 / yr"),
+                ("s1", "CH4", "R2", "MtCH4 / yr"),
+                ("s1", "Sulfur", "World", "MtS / yr"),
+            ],
+            names=["scenario", "species", "region", "unit"],
+        ),
+    )
+
+    scen_2 = pd.DataFrame(
+        [
+            [1.0, 1.2, 1.5],
+            [1.0, 1.0, 1.0],
+            [0.0, 0.2, 0.5],
+            [100.0, 200.0, 350.0],
+            [85.0, 200.0, 270.0],
+            [15.0, 0.0, 80.0],
+            [10.0, 23.0, 35.0],
+        ],
+        columns=[2020, 2040, 2050],
+        index=pd.MultiIndex.from_tuples(
+            [
+                ("s2", "CO2", "World", "GtC / yr"),
+                ("s2", "CO2", "R1", "GtC / yr"),
+                ("s2", "CO2", "R2", "GtC / yr"),
+                ("s2", "CH4", "World", "MtCH4 / yr"),
+                ("s2", "CH4", "R1", "MtCH4 / yr"),
+                ("s2", "CH4", "R2", "MtCH4 / yr"),
+                ("s2", "Sulfur", "World", "MtS / yr"),
+            ],
+            names=["scenario", "species", "region", "unit"],
+        ),
+    ).openscm.convert_unit(
+        {"MtCH4 / yr": "GtCH4 / yr", "MtS / yr": "MtSO2 / yr"},
+        ur=openscm_units.unit_registry,
+    )
+
+    scen_3 = pd.DataFrame(
+        [
+            [1.0, 0.2, -1.5],
+            [1.0, 0.5, -1.0],
+            [0.0, -0.3, -0.5],
+            [10.0, 10.0, 5.0],
+        ],
+        columns=[2020, 2040, 2050],
+        index=pd.MultiIndex.from_tuples(
+            [
+                ("s3", "CO2", "World", "GtC / yr"),
+                ("s3", "CO2", "R1", "GtC / yr"),
+                ("s3", "CO2", "R2", "GtC / yr"),
+                ("s3", "Sulfur", "World", "MtS / yr"),
+            ],
+            names=["scenario", "species", "region", "unit"],
+        ),
+    ).openscm.convert_unit(
+        pd.Series(
+            ["MtCO2 / yr"],
+            index=pd.MultiIndex.from_tuples(
+                [("s3", "CO2", "R2")], names=["scenario", "species", "region"]
+            ),
+        ),
+        ur=openscm_units.unit_registry,
+    )
+
+    db = pd.concat([scen_1, scen_2, scen_3])
+
+    def get_db():
+        return db
+
+    assert False, "Up to here"
+    infill(scen_input)
+
+    # Check that expected timeseries are all in output
+
+    # Check output values against doing it vs. siliecone by hand
+    # Put all the difficult edge cases in here, except errors
+    # Have specific tests for the rest
