45a9250506ff8d9dd9c4361361cf900d7d7a8f97
Marco Zecchetto <zecchetto@iiasa.ac.at>
2026-03-05 16:17:20 +0100

    forced aneris_overrides to DataFrame

--- src/gcages/cmip7_scenariomip/harmonisation.py
+++ src/gcages/cmip7_scenariomip/harmonisation.py
@@ -145,6 +145,7 @@ def create_cmip7_scenariomip_global_harmoniser(
     )
 
     aneris_overrides = load_aneris_overrides_file(aneris_global_overrides_file)
+    aneris_overrides = aneris_overrides.to_frame()  # from Series to DataFrame
     aneris_overrides = update_index_levels_func(
         aneris_overrides,
         {
