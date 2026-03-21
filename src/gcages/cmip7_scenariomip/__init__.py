44bbd2cad24ae2a71ce3c23d586167d125d4acb4
Marco Zecchetto <zecchetto@iiasa.ac.at>
2026-03-21 18:01:46 +0100

    Not really working: ensamble members are different

--- src/gcages/cmip7_scenariomip/__init__.py
+++ src/gcages/cmip7_scenariomip/__init__.py
@@ -16,11 +16,15 @@ from gcages.cmip7_scenariomip.pre_processing import (
     ReaggregatorBasic,
     ReaggregatorLike,
 )
+from gcages.cmip7_scenariomip.scm_running import (
+    CMIP7_SCENARIOMIP_SCMRunner,
+)
 
 __all__ = [
     "CMIP7ScenarioMIPInfiller",
     "CMIP7ScenarioMIPPreProcessingResult",
     "CMIP7ScenarioMIPPreProcessor",
+    "CMIP7_SCENARIOMIP_SCMRunner",
     "ReaggregatorBasic",
     "ReaggregatorLike",
     "create_cmip7_scenariomip_global_harmoniser",
---
+++ tests/regression/cmip7-scenariomip/test_regression_cmip7_scenariomip_scm_runnig.py
@@ -0,0 +1,113 @@
+"""
+Test infilling compared for CMIP7 ScenarioMIP
+"""
+
+from __future__ import annotations
+
+import multiprocessing
+from pathlib import Path
+
+import pytest
+from pandas_openscm.io import load_timeseries_csv
+
+from gcages.cmip7_scenariomip.harmonisation import (
+    load_cmip7_scenariomip_historical_emissions,
+)
+from gcages.cmip7_scenariomip.scm_running import (
+    CMIP7_SCENARIOMIP_SCMRunner,
+)
+from gcages.testing import (
+    KEY_CMIP7_SCENARIOMIP_TESTING_MODEL_SCENARIOS,
+    assert_frame_equal,
+    get_key_testing_model_scenario_parameters,
+)
+
+pix = pytest.importorskip("pandas_indexing")
+
+CMIP7_SCENARIOMIP_OUT_DIR = Path(__file__).parents[0] / "cmip7-scenariomip-output"
+
+PROCESSED_CMIP7_SCENARIOMIP_INPUT_DIR = (
+    Path(__file__).parents[0] / "cmip7-scenariomip-workflow-inputs"
+)
+CMIP7_SCENARIOMIP_HISTORICAL_GLOBAL_EMISSIONS_FILE = (
+    PROCESSED_CMIP7_SCENARIOMIP_INPUT_DIR / "history_cmip7_scenariomip.csv"
+)
+
+MAGIC_EXE = PROCESSED_CMIP7_SCENARIOMIP_INPUT_DIR / "magicc-v7.6.0a3/bin/magicc"
+MAGICC_CMIP7_PROBABILISTIC_CONFIG_FILE = (
+    PROCESSED_CMIP7_SCENARIOMIP_INPUT_DIR
+    / "magicc-v7.6.0a3/configs/magicc-ar7-fast-track-drawnset-v0-3-0.json"
+)
+
+HARMONISATION_YEAR = 2023
+
+
+@pytest.mark.skip_ci_default
+@pytest.mark.slow
+@get_key_testing_model_scenario_parameters(
+    KEY_CMIP7_SCENARIOMIP_TESTING_MODEL_SCENARIOS
+)
+def test_individual_scenario(model, scenario):
+    # Load history
+    historical_emissions = load_cmip7_scenariomip_historical_emissions(
+        filepath=CMIP7_SCENARIOMIP_HISTORICAL_GLOBAL_EMISSIONS_FILE,
+        check_hash=True,
+    )
+
+    # Loading infilled results
+    file = CMIP7_SCENARIOMIP_OUT_DIR / f"{model}_{scenario}_infilled.csv"
+    infilled = load_timeseries_csv(
+        file,
+        lower_column_names=True,
+        index_columns=["model", "scenario", "region", "variable", "unit"],
+        out_columns_type=int,
+    )
+    # exp = exp.loc[:, HARMONISATION_YEAR:2100]
+    # infilled.columns.name = "year"
+    # Select scenario and drop aggregated/cumulative rows
+    infilled = infilled.loc[
+        pix.ismatch(scenario=scenario)
+        & ~pix.ismatch(variable=["**Kyoto**", "Cumulative**", "**CO2", "**GHG**"])
+    ]
+
+    # Loading expected results
+    file = CMIP7_SCENARIOMIP_OUT_DIR / f"{model}_{scenario}_GSAT.csv"
+    exp_temperature = load_timeseries_csv(
+        file,
+        lower_column_names=True,
+        index_columns=[
+            "climate_model",
+            "model",
+            "region",
+            "run_id",
+            "scenario",
+            "unit",
+            "variable",
+        ],
+        out_columns_type=int,
+    )
+    # exp = exp.loc[:, HARMONISATION_YEAR:2100]
+    # exp_temperature.columns.name = "time"
+
+    scm_runner = CMIP7_SCENARIOMIP_SCMRunner.from_cmip7_scenariomip_config(
+        magicc_exe_path=MAGIC_EXE,
+        magicc_prob_distribution_path=MAGICC_CMIP7_PROBABILISTIC_CONFIG_FILE,
+        output_variables=("Surface Air Temperature Change",),
+        historical_emissions=historical_emissions,
+        harmonisation_year=2023,
+        n_processes=multiprocessing.cpu_count(),
+    )
+
+    # post_processor = AR6PostProcessor.from_ar6_config(n_processes=None)
+
+    scm_results = scm_runner(infilled)
+
+    assert_frame_equal(
+        scm_results[
+            scm_results.index.get_level_values("variable").str.contains(
+                "Surface Air Temperature Change"
+            )
+        ],
+        exp_temperature,
+        rtol=1e-1,
+    )
